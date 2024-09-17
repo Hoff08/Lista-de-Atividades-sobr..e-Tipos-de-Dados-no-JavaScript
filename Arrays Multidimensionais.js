962c80fb4cd45d89c7b876ac97c3d48be46ff045
Vicenzo Hoffmann <139232852+Hoff08@users.noreply.github.com>
2024-09-17 10:55:46 -0300

    Create Arrays Multidimensionais.js

---
+++ Arrays Multidimensionais.js
@@ -0,0 +1,15 @@
+// Declare a matriz
+let matriz = [
+  [1, 2, 3],
+  [4, 5, 6]
+];
+
+// Exiba a matriz
+console.log(matriz);
+
+// Acesse o valor na segunda linha, terceira coluna
+console.log(matriz[1][2]); // Saída: 6
+
+// Modifique o valor na primeira linha, segunda coluna
+matriz[0][1] = 99;
+console.log(matriz); // Exiba a matriz atualizada
