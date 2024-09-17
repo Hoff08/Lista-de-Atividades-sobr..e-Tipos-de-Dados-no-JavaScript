203576e90fb2e10073becafd78213c9f212736d1
Vicenzo Hoffmann <139232852+Hoff08@users.noreply.github.com>
2024-09-17 10:54:14 -0300

    Create Trabalhando com Null e Undefined.js

---
+++ Trabalhando com Null e Undefined.js
@@ -0,0 +1,13 @@
+// Declare a variável vazia
+let vazia = null;
+
+// Verifique se vazia é igual a undefined (==) e estritamente igual a undefined (===)
+console.log(vazia == undefined); // Saída: true
+console.log(vazia === undefined); // Saída: false
+
+// Declare a variável naoDefinida
+let naoDefinida;
+
+// Verifique se naoDefinida é igual a undefined (==) e estritamente igual a undefined (===)
+console.log(naoDefinida == undefined); // Saída: true
+console.log(naoDefinida === undefined); // Saída: true
