2e99f210746658b8539a781486ab00e1331ef34d
Vicenzo Hoffmann <139232852+Hoff08@users.noreply.github.com>
2024-09-17 10:54:49 -0300

    Create Manipulação de BigInt.js

---
+++ Manipulação de BigInt.js
@@ -0,0 +1,12 @@
+// Declare as variáveis grandeNumero1 e grandeNumero2
+let grandeNumero1 = BigInt(123456789012345678901234567890);
+let grandeNumero2 = BigInt(987654321098765432109876543210);
+
+// Calcule as operações
+console.log(grandeNumero1 + grandeNumero2); // Soma
+console.log(grandeNumero1 - grandeNumero2); // Subtração
+console.log(grandeNumero1 * grandeNumero2); // Multiplicação
+console.log(grandeNumero1 / grandeNumero2); // Divisão
+
+// Verifique se grandeNumero1 é maior que grandeNumero2
+console.log(grandeNumero1 > grandeNumero2); // Saída: false
